a865b9fdd36c47a03a73bec5627f2b115c690238
its-dibo <sh.eldeeb.2010@gmail.com>
2026-04-22 07:40:56 +0200

    style(prayer-time-nextjs): NavBar

---
+++ apps/blog-nextjs/src/components/NavBar.tsx
@@ -0,0 +1,20 @@
+import "./NavBar.css";
+
+export function NavBar() {
+  return (
+    <nav>
+      {/* logo */}
+      <div>
+        <span>SuperBlog</span>
+      </div>
+
+      {/* links */}
+      <div></div>
+
+      {/* profile */}
+      <div>
+        <span>profile</span>
+      </div>
+    </nav>
+  );
+}
